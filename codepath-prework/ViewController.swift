5516adf110713e4bce8a2bc6e304382143f7e9bf
Tony Liu <tliu5175@gmail.com>
2022-01-07 00:09:56 -0800

    finished the initial UI

---
+++ codepath-prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  codepath-prework
+//
+//  Created by Tony Liu on 1/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ codepath-preworkUITests/codepath_preworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  codepath_preworkUITestsLaunchTests.swift
+//  codepath-preworkUITests
+//
+//  Created by Tony Liu on 1/6/22.
+//
+
+import XCTest
+
+class codepath_preworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
